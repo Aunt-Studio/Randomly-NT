821919e45f488df7f74a97334c8de3f94e6037f7
yangnuozhen <56541171+yangnuozhen@users.noreply.github.com>
2025-01-26 01:17:23 +0800

    feat: 添加随机化因子显示功能
    在 `SettingsPage.xaml` 文件中，添加了一个新的 `ItemsSource` 绑定到 `RandomizationFactorItems`，并且为 `SettingsExpander` 添加了一个 `ItemTemplate`，用于显示随机化因子的详细信息，包括名称、描述和一个指向文档的超链接按钮。
    
    在 `SettingsPage.xaml.cs` 文件中，添加了 `System.Collections.ObjectModel` 的引用。
    
    在 `SettingsPage.xaml.cs` 文件的 `SettingsPage` 类中，添加了一个 `ObservableCollection<RandomizationFactorItem>` 类型的 `RandomizationFactorItems` 属性，并在构造函数中初始化了一个用于测试的 `RandomizationFactorItem` 对象。
    
    在 `SettingsPage.xaml.cs` 文件中，新增了一个 `RandomizationFactorItem` 类，包含 `Name`、`Description` 和 `DocumentUrl` 属性。

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -7,6 +7,7 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -27,17 +28,32 @@ namespace Randomly_NT
     {
         public string AssemblyVersion { get; }
         public int RandomizeIndex { get; set; } = 0;
+        public ObservableCollection<RandomizationFactorItem> RandomizationFactorItems = [];
         public SettingsPage()
         {
             this.InitializeComponent();
             AssemblyVersion = "Assembly Version ";
             AssemblyVersion += Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "未知程序集版本";
             this.DataContext = this;
+            // 测试数据
+            RandomizationFactorItems.Add(new RandomizationFactorItem
+            {
+                Name = "自动随机数种子",
+                Description = "系统 API 提供的自动随机数种子, 即 CPU 时钟",
+                DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+            });
         }
 
         private void RandomizeIndexSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
         {
             Debug.WriteLine(RandomizeIndex);
         }
+    }
+
+    public class RandomizationFactorItem
+    {
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public string? DocumentUrl { get; set; }
     }
 }
