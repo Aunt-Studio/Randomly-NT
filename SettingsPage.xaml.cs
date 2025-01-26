22a7ae876329bacf86fbe6bfc4ad8af114f38d7f
yangnuozhen <56541171+yangnuozhen@users.noreply.github.com>
2025-01-26 13:48:12 +0800

    feat: 添加本地设置持久化

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -5,6 +5,7 @@ using Microsoft.UI.Xaml.Data;
 using Microsoft.UI.Xaml.Input;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
+using Windows.Storage;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -27,9 +28,29 @@ namespace Randomly_NT
     /// </summary>
     public sealed partial class SettingsPage : Page, INotifyPropertyChanged
     {
+        /// <summary>
+        /// 本地设置容器
+        /// </summary>
+        private ApplicationDataContainer LocalSettings;
+
+        /// <summary>
+        /// 属性更改事件回调函数
+        /// </summary>
         public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// 用以 UI 展示的程序集版本
+        /// </summary>
         public string AssemblyVersion { get; }
+
+        /// <summary>
+        /// 随机性指数 字段
+        /// </summary>
         private int _randomizeIndex = 0;
+
+        /// <summary>
+        /// 随机性指数 属性
+        /// </summary>
         public int RandomizeIndex { 
             get => _randomizeIndex;
             set
@@ -41,7 +62,14 @@ namespace Randomly_NT
                     ConfigureRandomizationFactors();
                 }
             } }
+
+        /// <summary>
+        /// 随机性熵源前端展示列表 字段
+        /// </summary>
         private ObservableCollection<RandomizationFactorItem> _randomizationFactorItems = new();
+        /// <summary>
+        /// 随机性熵源前端展示列表 属性
+        /// </summary>
         public ObservableCollection<RandomizationFactorItem> RandomizationFactorItems { 
             get => _randomizationFactorItems;
             set
@@ -50,18 +78,41 @@ namespace Randomly_NT
                 OnPropertyChanged(nameof(RandomizationFactorItems));
             } 
         }
+
+
         public SettingsPage()
         {
             this.InitializeComponent();
+            // 获取本地设置容器
+            LocalSettings = ApplicationData.Current.LocalSettings;
+            // 读取随机性指数
+            _randomizeIndex = LocalSettings.Values.ContainsKey("RandomizeIndex") ? (int)LocalSettings.Values["RandomizeIndex"] : 1;
+
+            // 获取程序集版本
             AssemblyVersion = "Assembly Version ";
             AssemblyVersion += Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "未知程序集版本";
+
+            // 设置数据上下文
             this.DataContext = this;
+
+            // 配置随机性熵源前端列表展示
             ConfigureRandomizationFactors();
         }
+
         private void OnPropertyChanged(string propertyName)
         {
+            // 通知前端属性更改
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            // 保存随机性指数设置更改
+            if (propertyName == nameof(RandomizeIndex))
+            {
+                LocalSettings.Values["RandomizeIndex"] = RandomizeIndex;
+            }
         }
+
+        /// <summary>
+        /// 配置随机性熵源前端展示列表
+        /// </summary>
         private void ConfigureRandomizationFactors()
         {
             switch (RandomizeIndex)
@@ -151,12 +202,11 @@ namespace Randomly_NT
             }
         }
 
-        //private void RandomizeIndexSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
-        //{
-            
-        //}
     }
 
+    /// <summary>
+    /// 随机性熵源前端展示列表项
+    /// </summary>
     public class RandomizationFactorItem
     {
         public string? Name { get; set; }
