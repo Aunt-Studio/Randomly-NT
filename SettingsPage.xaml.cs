6d29cae4fc2fabf362f103d207f0934807028f56
yangnuozhen <56541171+yangnuozhen@users.noreply.github.com>
2025-01-26 13:20:26 +0800

    feat: 完成熵源前端展示

--- SettingsPage.xaml.cs
+++ SettingsPage.xaml.cs
@@ -8,6 +8,7 @@ using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
@@ -24,30 +25,136 @@ namespace Randomly_NT
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class SettingsPage : Page
+    public sealed partial class SettingsPage : Page, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler? PropertyChanged;
         public string AssemblyVersion { get; }
-        public int RandomizeIndex { get; set; } = 0;
-        public ObservableCollection<RandomizationFactorItem> RandomizationFactorItems = [];
+        private int _randomizeIndex = 0;
+        public int RandomizeIndex { 
+            get => _randomizeIndex;
+            set
+            {
+                if (_randomizeIndex != value) 
+                {
+                    _randomizeIndex = value;
+                    OnPropertyChanged(nameof(RandomizeIndex));
+                    ConfigureRandomizationFactors();
+                }
+            } }
+        private ObservableCollection<RandomizationFactorItem> _randomizationFactorItems = new();
+        public ObservableCollection<RandomizationFactorItem> RandomizationFactorItems { 
+            get => _randomizationFactorItems;
+            set
+            {
+                _randomizationFactorItems = value;
+                OnPropertyChanged(nameof(RandomizationFactorItems));
+            } 
+        }
         public SettingsPage()
         {
             this.InitializeComponent();
             AssemblyVersion = "Assembly Version ";
             AssemblyVersion += Assembly.GetExecutingAssembly()?.GetName()?.Version?.ToString() ?? "未知程序集版本";
             this.DataContext = this;
-            // 测试数据
-            RandomizationFactorItems.Add(new RandomizationFactorItem
+            ConfigureRandomizationFactors();
+        }
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+        private void ConfigureRandomizationFactors()
+        {
+            switch (RandomizeIndex)
             {
-                Name = "自动随机数种子",
-                Description = "系统 API 提供的自动随机数种子, 即 CPU 时钟",
-                DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
-            });
+                case 0 or 1:
+                    RandomizationFactorItems = new ObservableCollection<RandomizationFactorItem>
+                    {
+                        new RandomizationFactorItem
+                        {
+                            Name = "默认随机数种子",
+                            Description = "系统 API 提供的默认随机数种子, 即 CPU 时钟。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        }
+                    };
+                    break;
+                case 2:
+                    RandomizationFactorItems = new ObservableCollection<RandomizationFactorItem>
+                    {
+                        new RandomizationFactorItem
+                        {
+                            Name = "默认随机数种子",
+                            Description = "系统 API 提供的默认随机数种子, 即 CPU 时钟。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        },
+                        new RandomizationFactorItem
+                        {
+                            Name = "运行时环境噪声",
+                            Description = "可能包含系统状态信息，如内存使用情况等。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        }
+                    };
+                    break;
+                case 3:
+                    RandomizationFactorItems = new ObservableCollection<RandomizationFactorItem>
+                    {
+                        new RandomizationFactorItem
+                        {
+                            Name = "默认随机数种子",
+                            Description = "系统 API 提供的默认随机数种子, 即 CPU 时钟。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        },
+                        new RandomizationFactorItem
+                        {
+                            Name = "运行时环境噪声",
+                            Description = "可能包含系统状态信息，如内存使用情况等。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        },
+                        new RandomizationFactorItem
+                        {
+                            Name = "鼠标指针",
+                            Description = "鼠标指针的位置。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        }
+                    };
+                    break;
+                case 4:
+                    RandomizationFactorItems = new ObservableCollection<RandomizationFactorItem>
+                    {
+                        new RandomizationFactorItem
+                        {
+                            Name = "默认随机数种子",
+                            Description = "系统 API 提供的默认随机数种子, 即 CPU 时钟。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        },
+                        new RandomizationFactorItem
+                        {
+                            Name = "运行时环境噪声",
+                            Description = "可能包含系统状态信息，如内存使用情况等。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        },
+                        new RandomizationFactorItem
+                        {
+                            Name = "鼠标指针",
+                            Description = "鼠标指针的位置。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        },
+                        new RandomizationFactorItem
+                        {
+                            Name = "真随机数接口 (需要访问网络)",
+                            Description = "从 Random.org 获取的利用大气噪声生成的真随机数。获取该熵可能会产生一定的时间开销。",
+                            DocumentUrl = "https://docs.microsoft.com/zh-cn/dotnet/api/system.random"
+                        }
+                    };
+                    break;
+                default:
+                    break;
+            }
         }
 
-        private void RandomizeIndexSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
-        {
-            Debug.WriteLine(RandomizeIndex);
-        }
+        //private void RandomizeIndexSlider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        //{
+            
+        //}
     }
 
     public class RandomizationFactorItem
